196687263682aa9679ab918b9320b149fe7bd92c
irismelendez <iris.13melendez@gmail.com>
2025-12-01 10:30:49 -0600

    I displayed products from northwind

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.sql.*;
 
 public class Main {
+
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind","root", "1022Bella" );
+
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products;");
+
+        while (resultSet.next()) {
+            int id = resultSet.getInt("ProductID");
+            String name = resultSet.getString("ProductName");
+            double price = resultSet.getDouble("UnitPrice");
+            int stock = resultSet.getInt("UnitsInStock");
+
+            System.out.println("Product Id: " + id);
+            System.out.println("Name: " + name);
+            System.out.println("Price: $" + String.format("%.2f", price));
+            System.out.println("Stock: " + stock);
+            System.out.println("------------------------");
+        }
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
 }
